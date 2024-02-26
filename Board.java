e27aacc5ea8ec5e0f7ee9ffa1ccdac0d29af11bf
Chris Sheridan <csheri46@gmail.com>
2024-02-26 03:54:11 -0500

    Board class done, implement solvers next

--- Board.java
+++ Board.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class Board {
     private int[][] tiles;
@@ -6,22 +9,18 @@ public class Board {
     private int[][] goal;
 
     //constructor
-    public Board(int[][] tiles){
+    public Board(int[][] tiles, int[][] goal){
         this.tiles = tiles;
         this.size = tiles.length;
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                goal[i][j] = i;
-            }
-            goal[size][size] = 0;
-        }
+        this.goal = goal;
+
     }
 
     // string representation of this board
     public String toString(){
         String result = "";
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < 1; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 result += tiles[i][j] + " ";
             }
             result += "\n";
@@ -39,7 +38,7 @@ public class Board {
         int hamming = 0;
          for (int i = 0; i < size; i++) {
              for (int j = 0; j < size; j++) {
-                 if(tiles[i][j] != goal[i][j]){
+                 if((tiles[i][j] != goal[i][j]) && tiles[i][j] != 0){
                      hamming++;
                  }
              }
@@ -50,7 +49,27 @@ public class Board {
      // sum of Manhattan distances between tiles and goal
      public int manhattan(){
         int manhattan = 0;
-        int moves;
+        int x0 = 0;
+        int x1 = 0;
+        int y0 = 0;
+        int y1 = 0;
+         for (int i = 0; i < size; i++) {
+             for (int j = 0; j < size; j++) {
+                 if ((tiles[i][j] != goal[i][j]) && tiles[i][j] != 0){
+                     x1 = i+1;
+                     y1 = j+1;
+                     for (int k = 0; k < size; k++) {
+                         for (int l = 0; l < size; l++) {
+                            if(goal[k][l] == tiles[x1-1][y1-1]){
+                                x0 = k+1;
+                                y0 = l+1;
+                            }
+                         }
+                     }
+                     manhattan += Math.abs(x1-x0) + Math.abs(y1-y0);
+                 }
+             }
+         }
 
 
         return manhattan;
@@ -67,7 +86,7 @@ public class Board {
 
      // all neighboring boards
      public Iterable<Board> neighbors(){
-        Collection <Board> neighbors = null;
+        List<Board> neighbors = new ArrayList<>();
         int xpos = 0;
         int ypos = 0;
         for (int i = 0; i < size; i++){
@@ -77,36 +96,63 @@ public class Board {
                     ypos = j;
                     break;
                 }
+                if (tiles[xpos][ypos] == 0){
+                    break;
+                }
+            }
+            if (tiles[xpos][ypos] == 0){
+                break;
             }
         }
         if (xpos != 0){
-            int [][] n1 = tiles.clone();
+            int [][] n1 = new int [size][size];
+            for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++) {
+                    n1[i][j] = tiles[i][j];
+                }
+            }
             n1[xpos][ypos] = n1[xpos-1][ypos];
             n1[xpos-1][ypos] = 0;
-            Board neighbor1 = new Board(n1);
+            Board neighbor1 = new Board(n1, goal);
             neighbors.add(neighbor1);
         }
         if (ypos!= 0){
-            int [][] n2 = tiles.clone();
+            int [][] n2 = new int [size][size];
+            for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++) {
+                    n2[i][j] = tiles[i][j];
+                }
+            }
             n2[xpos][ypos] = n2[xpos][ypos-1];
             n2[xpos][ypos-1] = 0;
-            Board neighbor2 = new Board(n2);
+            Board neighbor2 = new Board(n2, goal);
             neighbors.add(neighbor2);
         }
-        if (xpos < size){
-            int [][] n3 = tiles.clone();
+        if (xpos+1 < size){
+            int [][] n3 = new int [size][size];
+            for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++) {
+                    n3[i][j] = tiles[i][j];
+                }
+            }
             n3[xpos][ypos] = n3[xpos+1][ypos];
             n3[xpos+1][ypos] = 0;
-            Board neighbor3 = new Board(n3);
+            Board neighbor3 = new Board(n3, goal);
             neighbors.add(neighbor3);
         }
-        if (ypos < size){
-            int [][] n4 = tiles.clone();
+        if (ypos+1 < size){
+            int [][] n4 = new int [size][size];
+            for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++) {
+                    n4[i][j] = tiles[i][j];
+                }
+            }
             n4[xpos][ypos] = n4[xpos][ypos+1];
             n4[xpos][ypos+1] = 0;Board
-            neighbor4 = new Board(n4);
+            neighbor4 = new Board(n4, goal);
             neighbors.add(neighbor4);
         }
         return neighbors;
      }
+
 }
--- main.java
+++ main.java
@@ -1,6 +1,75 @@
+import java.util.*;
+import java.io.*;
 public class main {
 
     public static void main(String [] args){
-                
+        String fileName = "";
+        Scanner input = new Scanner(System.in);
+        int pSize;
+        int tempInt;
+        int count = 0;
+        int moves;
+        char method = ' ';
+        System.out.print("Input filename of puzzle to be solved: ");
+        fileName = input.next();
+        File f = new File(fileName);
+        try{
+            Scanner read = new Scanner(f);
+            pSize = read.nextInt();
+            int [][] puzzle = new int[pSize][pSize];
+            int [][] goal = new int[pSize][pSize];
+            String data = "";
+
+            while (read.hasNext()) {
+                data += read.next();
+            }
+            for (int i = 0; i < pSize; i++){
+                for (int j = 0; j < pSize; j++){
+                    tempInt = Character.getNumericValue(data.charAt(count));
+                    puzzle[i][j] = tempInt;
+                    count++;
+                }
+            }
+            count = 1;
+            for (int i = 0; i < pSize; i++){
+                for (int j = 0; j < pSize; j++){
+                    goal[i][j] = count;
+                    count++;
+                }
+                goal[pSize-1][pSize-1] = 0;
+            }
+
+            Board b = new Board(puzzle, goal);
+            System.out.print("Which solving method would you like to use? (A for A*, B for Divide and Conquer, " +
+                    "any other key to quit): ");
+            method = input.next().charAt(0);
+            if (method == 'A' ||method == 'a'){
+                aStarSolver(b);
+
+            }
+            else if (method == 'B' || method == 'b'){
+                divAndConqSolver(b);
+            }
+
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Error occurred");
+            e.printStackTrace();
+        }
+
+
     }
+
+    static void aStarSolver(Board initial){
+
+    }
+    static void divAndConqSolver (Board initial){
+
+    }
+    public boolean isSolvable(){
+        boolean isSolvable = false;
+        return isSolvable;
+    }
+
+
 }
